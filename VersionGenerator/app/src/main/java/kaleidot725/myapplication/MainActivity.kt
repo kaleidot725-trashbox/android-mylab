cdc43e4e9f85f1b0d688297f13836ff0e831f20a
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-10-31 21:38:38 +0900

    Update VersionGenerator

--- VersionGenerator/app/src/main/java/kaleidot725/myapplication/MainActivity.kt
+++ VersionGenerator/app/src/main/java/kaleidot725/myapplication/MainActivity.kt
@@ -2,11 +2,18 @@ package kaleidot725.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val packageInfo = packageManager.getPackageInfo(packageName, 0);
+        val version = packageInfo.versionName
+
+        val versionView = findViewById<TextView>(R.id.version_text)
+        versionView.text = version
     }
 }
