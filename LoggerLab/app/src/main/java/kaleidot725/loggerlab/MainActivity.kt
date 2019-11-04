6560081dcd8548b7156f1cf73ea0a107f63ffeb5
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-11-04 14:36:41 +0900

    Update LoggerLab

--- LoggerLab/app/src/main/java/kaleidot725/loggerlab/MainActivity.kt
+++ LoggerLab/app/src/main/java/kaleidot725/loggerlab/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import java.lang.Exception
 import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
@@ -12,24 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.println(Log.ASSERT, "MainActivity", "ASSERT")
+        Log.v("TAG", "Message")
+
+        try {
+            throw Exception("Unknown Error")
+        } catch (e : Exception) {
+            Log.v("TAG", "Message", e)
+        }
+
         val verbose = findViewById<Button>(R.id.verbose_button)
         verbose.setOnClickListener {
             Log.v("MainActivity", "verbose")
-        }
-
-        val warn = findViewById<Button>(R.id.warn_button)
-        warn.setOnClickListener {
-            Log.w("MainActivity", "warn")
-        }
-
-        val info = findViewById<Button>(R.id.info_button)
-        info.setOnClickListener {
-            Log.i("MainActivity", "info")
-        }
-
-        val assert = findViewById<Button>(R.id.assert_button)
-        assert.setOnClickListener {
-            Log.wtf("MainActvity", "Assert")
         }
 
         val debug = findViewById<Button>(R.id.debug_button)
@@ -37,9 +32,40 @@ class MainActivity : AppCompatActivity() {
             Log.d("MainActivity", "debug")
         }
 
+        val info = findViewById<Button>(R.id.info_button)
+        info.setOnClickListener {
+            Log.i("MainActivity", "info")
+        }
+
+        val warn = findViewById<Button>(R.id.warn_button)
+        warn.setOnClickListener {
+            Log.w("MainActivity", "warn")
+        }
+
         val error = findViewById<Button>(R.id.error_button)
         error.setOnClickListener {
             Log.e("MainActivity", "error")
         }
+
+        val assert = findViewById<Button>(R.id.assert_button)
+        assert.setOnClickListener {
+            Log.println(Log.ASSERT, "MainActivity", "Assert")
+        }
+
+        val print = findViewById<Button>(R.id.println_button)
+        print.setOnClickListener {
+            println("Println")
+        }
+
+        if (Log.isLoggable("MainActivity", Log.DEBUG)) {
+            Log.d("MainActivity", "Good Debug Log")
+        }
+
+        Log.println(Log.ASSERT, "MainAcitivty", "VERBOSE : ${Log.isLoggable("MainActivity", Log.VERBOSE)}")
+        Log.println(Log.ASSERT, "MainAcitivty", "DEBUG   : ${Log.isLoggable("MainActivity", Log.DEBUG)}")
+        Log.println(Log.ASSERT, "MainAcitivty", "INFO    : ${Log.isLoggable("MainActivity", Log.INFO)}")
+        Log.println(Log.ASSERT, "MainAcitivty", "WARN    : ${Log.isLoggable("MainActivity", Log.WARN)}")
+        Log.println(Log.ASSERT, "MainAcitivty", "ERROR   : ${Log.isLoggable("MainActivity", Log.ERROR)}")
+        Log.println(Log.ASSERT, "MainAcitivty", "ASSERT  : ${Log.isLoggable("MainActivity", Log.ASSERT)}")
     }
 }
