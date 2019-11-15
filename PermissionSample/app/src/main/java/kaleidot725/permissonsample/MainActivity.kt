18d1b7b75ee124d19d7fe476ba42d5b6c25c83a1
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-11-16 01:58:01 +0900

    Add PermissionSample

--- PermissionSample/app/src/main/java/kaleidot725/permissonsample/MainActivity.kt
+++ PermissionSample/app/src/main/java/kaleidot725/permissonsample/MainActivity.kt
@@ -1,12 +1,60 @@
 package kaleidot725.permissonsample
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+            recreate()
+            return
+        }
+
+        textView = findViewById(R.id.message_textview)
+        updateWifiStatus()
+        requestLocationUpdates()
+    }
+
+    private fun requestLocationUpdates() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            val locationListener = object : LocationListener {
+                override fun onLocationChanged(location: Location) {
+                    textView.text = location.toString()
+                }
+
+                override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+                override fun onProviderEnabled(provider: String) {}
+                override fun onProviderDisabled(provider: String) {}
+            }
+
+            val locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000.toLong(), 1.toFloat(), locationListener)
+        }
+    }
+
+    private fun updateWifiStatus()  {
+        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = cm.activeNetwork
+        if (network != null) {
+            val capa = cm.getNetworkCapabilities(network)
+            textView.text =  if (capa.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) "Wifi接続中" else "Wifi切断中"
+        }
     }
 }
