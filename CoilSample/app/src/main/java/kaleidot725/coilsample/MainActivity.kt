74b34416f5870692752e349bf1bf6d0b24234f4a
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-11-28 23:57:31 +0900

    Update CoilSample

--- CoilSample/app/src/main/java/kaleidot725/coilsample/MainActivity.kt
+++ CoilSample/app/src/main/java/kaleidot725/coilsample/MainActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import coil.Coil
+import coil.ImageLoader
 import coil.api.load
-import coil.transform.BlurTransformation
-import coil.transform.CircleCropTransformation
-import coil.transform.Transformation
+import coil.transform.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,12 +22,14 @@ class MainActivity : AppCompatActivity() {
             val imageView = findViewById<ImageView>(R.id.image_view)
             val reloadButton = findViewById<Button>(R.id.reload_button)
             reloadButton.setOnClickListener {
-                imageView.load(url)
+                val disposable = imageView.load(url)
+                disposable.dispose()
             }
         }
 
         fun loadCrossfade() {
-            val url = "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
+            val url =
+                "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
             val imageView = findViewById<ImageView>(R.id.image_view)
             val reloadButton = findViewById<Button>(R.id.reload_button)
             reloadButton.setOnClickListener {
@@ -38,7 +40,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         fun loadCrossfadeMs() {
-            val url = "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
+            val url =
+                "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
             val imageView = findViewById<ImageView>(R.id.image_view)
             val reloadButton = findViewById<Button>(R.id.reload_button)
             reloadButton.setOnClickListener {
@@ -49,7 +52,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         fun loadPlaceHolder() {
-            val url = "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
+            val url =
+                "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
             val imageView = findViewById<ImageView>(R.id.image_view)
             val reloadButton = findViewById<Button>(R.id.reload_button)
             reloadButton.setOnClickListener {
@@ -72,25 +76,98 @@ class MainActivity : AppCompatActivity() {
         }
 
         fun loadBlur() {
-            val url = "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
+            val url =
+                "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
             val imageView = findViewById<ImageView>(R.id.image_view)
             val reloadButton = findViewById<Button>(R.id.reload_button)
             reloadButton.setOnClickListener {
                 imageView.load(url) {
-                    transformations(BlurTransformation(context = applicationContext, radius = 20f, sampling = 5f))
+                    transformations(
+                        BlurTransformation(
+                            context = applicationContext,
+                            radius = 20f,
+                            sampling = 5f
+                        )
+                    )
                 }
             }
         }
 
-        val url = "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
-        val imageView = findViewById<ImageView>(R.id.image_view)
-        val reloadButton = findViewById<Button>(R.id.reload_button)
-        reloadButton.setOnClickListener {
-            imageView.load(url) {
-                transformations(CircleCropTransformation())
+        fun loadCircleCrop() {
+            val url =
+                "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
+            val imageView = findViewById<ImageView>(R.id.image_view)
+            val reloadButton = findViewById<Button>(R.id.reload_button)
+            reloadButton.setOnClickListener {
+                imageView.load(url) {
+                    transformations(CircleCropTransformation())
+                }
             }
         }
 
+        fun loadGrayScale() {
+            val url =
+                "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
+            val imageView = findViewById<ImageView>(R.id.image_view)
+            val reloadButton = findViewById<Button>(R.id.reload_button)
+            reloadButton.setOnClickListener {
+                imageView.load(url) {
+                    transformations(GrayscaleTransformation())
+                }
+            }
+        }
+
+        fun loadRoundedCorners() {
+            val url =
+                "https://www.underconsideration.com/brandnew/archives/android_2019_logo_inverse.png"
+            val imageView = findViewById<ImageView>(R.id.image_view)
+            val reloadButton = findViewById<Button>(R.id.reload_button)
+            reloadButton.setOnClickListener {
+                imageView.load(url) {
+                    transformations(
+                        RoundedCornersTransformation(
+                            topRight = 10f,
+                            topLeft = 10f,
+                            bottomLeft = 10f,
+                            bottomRight = 10f
+                        )
+                    )
+                }
+            }
+
+        }
+
+        fun loadUsingImageLoader() {
+            val imageLoader = ImageLoader(applicationContext) {
+                crossfade(true)
+                placeholder(R.drawable.placeholder)
+                error(R.drawable.error)
+                availableMemoryPercentage(0.1)
+                bitmapPoolPercentage(0.1)
+            }
+            val url = "https://notfound.png"
+            val imageView = findViewById<ImageView>(R.id.image_view)
+            val reloadButton = findViewById<Button>(R.id.reload_button)
+            reloadButton.setOnClickListener {
+                imageView.load(url, imageLoader)
+            }
+        }
+
+
+        Coil.setDefaultImageLoader(ImageLoader(applicationContext) {
+            crossfade(true)
+            placeholder(R.drawable.placeholder)
+            error(R.drawable.error)
+            availableMemoryPercentage(0.1)
+            bitmapPoolPercentage(0.1)
+        })
+
+        val url = "https://notfound.png"
+        val imageView = findViewById<ImageView>(R.id.image_view)
+        val reloadButton = findViewById<Button>(R.id.reload_button)
+        reloadButton.setOnClickListener {
+            imageView.load(url)
+        }
     }
 }
 
