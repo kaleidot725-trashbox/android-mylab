1fa03dc48c30789c04b4806b78f3a85fa8612388
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-11-30 13:48:09 +0900

    Update ExoPlayerSample

--- ExoPlayerSample/app/src/main/java/kaleidot725/exoplayersample/MainActivity.kt
+++ ExoPlayerSample/app/src/main/java/kaleidot725/exoplayersample/MainActivity.kt
@@ -1,10 +1,19 @@
 package kaleidot725.exoplayersample
 
+import android.media.MediaCodecList
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.exoplayer2.DefaultRenderersFactory
 import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.RenderersFactory
+import com.google.android.exoplayer2.mediacodec.MediaCodecInfo
+import com.google.android.exoplayer2.mediacodec.MediaCodecSelector
+import com.google.android.exoplayer2.mediacodec.MediaCodecUtil
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelector
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
@@ -16,8 +25,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val customCodecSelector = object : MediaCodecSelector {
+            override fun getDecoderInfos(mimeType: String?, requiresSecureDecoder: Boolean, requiresTunnelingDecoder: Boolean): MutableList<MediaCodecInfo> {
+                return MediaCodecUtil.getDecoderInfos(mimeType, requiresSecureDecoder, requiresTunnelingDecoder).toMutableList().filter {
+                    it.name.startsWith("OMX.google.")
+                }.toMutableList()
+            }
+
+            override fun getPassthroughDecoderInfo(): MediaCodecInfo? {
+                return MediaCodecUtil.getPassthroughDecoderInfo()
+            }
+        }
+
         val view = findViewById<PlayerView>(R.id.player_view)
-        val player = ExoPlayerFactory.newSimpleInstance(this).apply {
+        val trackSelector = DefaultTrackSelector()
+        val renderersFactory = DefaultRenderersFactory(this).apply {
+            setMediaCodecSelector(customCodecSelector)
+        }
+        val player = ExoPlayerFactory.newSimpleInstance(this, renderersFactory, trackSelector).apply {
             val uri = Uri.parse("https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4")
             val dataSource = DefaultDataSourceFactory(this@MainActivity, Util.getUserAgent(this@MainActivity, "ExoPlayerSample"))
             val videoSource = ProgressiveMediaSource.Factory(dataSource).createMediaSource(uri)
