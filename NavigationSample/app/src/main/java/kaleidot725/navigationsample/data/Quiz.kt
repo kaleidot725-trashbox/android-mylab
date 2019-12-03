749b883621be1823517a2d1daf64668ea91bf04d
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-12-03 23:43:50 +0900

    Update NavigationSample

--- NavigationSample/app/src/main/java/kaleidot725/navigationsample/data/Quiz.kt
+++
@@ -1,5 +0,0 @@
-package kaleidot725.navigationsample.data
-
-class Quiz(val id : Int, val title :  String, val choices : List<String>, private val answer : String) {
-    fun result(a : String) : Boolean = (a == answer)
-}
