cd8d7a3b929e1c1eb78a599cb6f59871b0c99e69
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-12-08 19:55:58 +0900

    Add SafeArgsSample

---
+++ SafeArgsSample/app/src/main/java/c/kaleidot725/safeargssample/ThreeFragment.kt
@@ -0,0 +1,18 @@
+package c.kaleidot725.safeargssample
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ThreeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_three, container, false)
+    }
+}
