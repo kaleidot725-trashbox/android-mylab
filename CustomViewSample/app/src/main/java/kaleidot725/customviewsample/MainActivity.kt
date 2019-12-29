c79e799982e9dd93694e3306105778223f9ec7e5
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-12-29 18:39:12 +0900

    Update CustomViewSample

--- CustomViewSample/app/src/main/java/kaleidot725/customviewsample/MainActivity.kt
+++ CustomViewSample/app/src/main/java/kaleidot725/customviewsample/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numberView : NumberView = findViewById(R.id.number_view)
+        numberView.setNumber(999999)
+        numberView.setDisplayMode(0)
     }
 }
--- CustomViewSample/app/src/main/java/kaleidot725/customviewsample/NumberView.kt
+++ CustomViewSample/app/src/main/java/kaleidot725/customviewsample/NumberView.kt
@@ -1,6 +1,10 @@
 package kaleidot725.customviewsample
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 
@@ -16,6 +20,56 @@ class NumberView(context : Context, attributeSet: AttributeSet) : View(context, 
             } finally {
                 recycle()
             }
+        }
+    }
+
+    fun setNumber(number : Int) {
+        this.number = number
+        this.invalidate()
+        this.requestLayout()
+    }
+
+    fun setDisplayMode(displayMode: Int) {
+        this.displayMode = displayMode
+        this.invalidate()
+        this.requestLayout()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        val backgroundPaint = Paint().apply {
+            setColor(Color.GREEN)
+        }
+        canvas?.drawRect(0f, 0f, this.width.toFloat(), this.height.toFloat(), backgroundPaint)
+
+        val textPaint = Paint().apply {
+            setColor(Color.BLACK)
+            textSize = 50f
+        }
+        val text = getDisplayNumber(number, displayMode)
+        val textWidth = getTextWidth(text, textPaint)
+        canvas?.drawText(text, (this.width / 2f) - (textWidth / 2), this.height / 2f, textPaint)
+    }
+
+    private fun getTextWidth(text : String, paint : Paint) : Float {
+        val bounds = Rect()
+        paint.getTextBounds(text, 0, text.length, bounds)
+        return bounds.width().toFloat() + bounds.left.toFloat()
+    }
+
+    private fun getDisplayNumber(number : Int, displayMode : Int) : String {
+        fun String.replace(vararg pairs: Pair<String, String>): String =
+            pairs.fold(this) { acc, (old, new) -> acc.replace(old, new, ignoreCase = true) }
+
+        return when(displayMode) {
+            0 -> { number.toString() }
+            1 -> { number.toString().replace(
+                "0" to "〇", "1" to "一", "2" to "二", "3" to "三", "4" to "四",
+                "5" to "五", "6" to "六", "7" to "七", "8" to "八", "9" to "九"
+            )
+            }
+            else -> { "error" }
         }
     }
 }
