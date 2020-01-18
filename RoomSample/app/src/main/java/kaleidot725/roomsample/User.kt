6ecf124b8129bc3821ccb6ec3f99857ffba903bf
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-01-18 11:17:27 +0900

    Add Ignore Property

--- RoomSample/app/src/main/java/kaleidot725/roomsample/User.kt
+++ RoomSample/app/src/main/java/kaleidot725/roomsample/User.kt
@@ -2,11 +2,13 @@ package kaleidot725.roomsample
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "users")
 data class User(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "first_name") val firstName: String?,
-    @ColumnInfo(name = "last_name") val lastName: String?
+    @ColumnInfo(name = "last_name") val lastName: String?,
+    @Ignore val age: Int
 )
