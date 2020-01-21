758938e63e78e4684397ad6168c84536b7fe6a65
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-01-21 22:59:23 +0900

    Update NavigationPopupSample

--- NavigationPopupSample/app/src/main/java/kaleidot725/navigationpopupsample/MainActivity.kt
+++ NavigationPopupSample/app/src/main/java/kaleidot725/navigationpopupsample/MainActivity.kt
@@ -2,11 +2,16 @@ package kaleidot725.navigationpopupsample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
     }
 }
