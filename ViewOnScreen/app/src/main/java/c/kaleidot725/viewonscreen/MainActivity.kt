fd3fe211d6e96c76591508c2767870ef3793e0a1
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-02-17 23:11:36 +0900

    Update ViewOnScreen

--- ViewOnScreen/app/src/main/java/c/kaleidot725/viewonscreen/MainActivity.kt
+++ ViewOnScreen/app/src/main/java/c/kaleidot725/viewonscreen/MainActivity.kt
@@ -1,12 +1,12 @@
 package c.kaleidot725.viewonscreen
 
-import android.app.Dialog
+import android.graphics.Color
 import android.graphics.Point
+import android.graphics.Rect
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.View
-import android.view.WindowManager
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,43 +16,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        location_in_window_button.setOnClickListener {
-            toastLocationInWindow(text_view)
-        }
-        location_on_screen_button.setOnClickListener {
-            toastLocationOnScreen(text_view)
-        }
 
-        Dialog(this).apply {
-            setContentView(R.layout.activity_main)
-            window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT)
-            show()
-
-            val textView = findViewById<TextView>(R.id.text_view)
-            val inWindowButton = findViewById<Button>(R.id.location_in_window_button)
-            val onScreenButton = findViewById<Button>(R.id.location_on_screen_button)
-            inWindowButton.setOnClickListener {
-                toastLocationInWindow(textView)
+        val oneTextView = one
+        val twoTextView = two
+        val threeTextView = three
+        scroll_view.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            val screenRect = getScreenRect()
+            val oneRect = oneTextView.getViewRect()
+            if (screenRect.contains(oneRect)) {
+                oneTextView.setBackgroundColor(Color.RED)
+            } else if (screenRect.intersect(oneRect)) {
+                oneTextView.setBackgroundColor(Color.BLUE)
             }
-            onScreenButton.setOnClickListener {
-                toastLocationOnScreen(textView)
+
+            val twoRect = twoTextView.getViewRect()
+            if (screenRect.contains(twoRect)) {
+                twoTextView.setBackgroundColor(Color.RED)
+            } else if (screenRect.intersect(twoRect)) {
+                twoTextView.setBackgroundColor(Color.BLUE)
             }
+
+            val threeRect = threeTextView.getViewRect()
+            if (screenRect.contains(threeRect)) {
+                threeTextView.setBackgroundColor(Color.RED)
+            } else if (screenRect.intersect(threeRect)) {
+                threeTextView.setBackgroundColor(Color.BLUE)
+            }
+
+            Log.v("TAG", "screen ${screenRect}")
+            Log.v("TAG", "one ${oneRect}")
+            Log.v("TAG", "two ${twoRect}")
+            Log.v("TAG", "three ${threeRect}")
         }
-
-    }
-
-    private fun toastAllViewLocationInWindow() {
-
-        var str = getViewLocationInWindowString("one", one) + "\n"
-        str += getViewLocationInWindowString("two", two) + "\n"
-        str += getViewLocationInWindowString("three", three) + "\n"
-        str += getViewLocationInWindowString("four", four) + "\n"
-        str += getViewLocationInWindowString("five", five) + "\n"
-        str += getViewLocationInWindowString("six", six) + "\n"
-        str += getViewLocationInWindowString("seven", seven) + "\n"
-        str += getViewLocationInWindowString("eight", eight) + "\n"
-        str += getViewLocationInWindowString("nine", nine) + "\n"
-        Toast.makeText(applicationContext, str, Toast.LENGTH_LONG).show()
     }
 
     private fun getViewLocationInWindowString(name: String, view: View): String {
@@ -60,16 +55,17 @@ class MainActivity : AppCompatActivity() {
         return "${name} inWindow ${inWindow}"
     }
 
+    private fun toastAllViewLocationInWindow() {
+        var str = getViewLocationInWindowString("one", one) + "\n"
+        str += getViewLocationInWindowString("two", two) + "\n"
+        str += getViewLocationInWindowString("three", three) + "\n"
+        Toast.makeText(applicationContext, str, Toast.LENGTH_LONG).show()
+    }
+
     private fun toastAllViewLocationOnScreen() {
         var str = getViewLocationOnScreenString("one", one) + "\n"
         str += getViewLocationOnScreenString("two", two) + "\n"
         str += getViewLocationOnScreenString("three", three) + "\n"
-        str += getViewLocationOnScreenString("four", four) + "\n"
-        str += getViewLocationOnScreenString("five", five) + "\n"
-        str += getViewLocationOnScreenString("six", six) + "\n"
-        str += getViewLocationOnScreenString("seven", seven) + "\n"
-        str += getViewLocationOnScreenString("eight", eight) + "\n"
-        str += getViewLocationOnScreenString("nine", nine) + "\n"
         Toast.makeText(applicationContext, str, Toast.LENGTH_LONG).show()
     }
 
@@ -90,13 +86,15 @@ class MainActivity : AppCompatActivity() {
         return Point(array[0], array[1])
     }
 
-    private fun toastLocationInWindow(view: View) {
-        val point = view.getLocationPointInWindow()
-        Toast.makeText(applicationContext, "X ${point.x} Y ${point.y}", Toast.LENGTH_LONG).show()
+    private fun getScreenRect(): Rect {
+        val point = Point()
+        windowManager.defaultDisplay.getSize(point)
+        return Rect(0, 0, point.x, point.y)
     }
 
-    private fun toastLocationOnScreen(view: View) {
-        val point = view.getLocationPointOnScreen()
-        Toast.makeText(applicationContext, "X ${point.x} Y ${point.y}", Toast.LENGTH_LONG).show()
+    private fun View.getViewRect(): Rect {
+        val array = IntArray(2)
+        this.getLocationOnScreen(array)
+        return Rect(array[0], array[1], array[0] + this.width, array[1] + this.height)
     }
 }
